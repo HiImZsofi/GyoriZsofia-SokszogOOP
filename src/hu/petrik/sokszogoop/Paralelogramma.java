a949b63ac168691294a68074fef63c76973608c2
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-04 15:52:53 +0200

    paralelogramma

---
+++ src/hu/petrik/sokszogoop/Paralelogramma.java
@@ -0,0 +1,19 @@
+package hu.petrik.sokszogoop;
+
+public class Paralelogramma extends Sokszog{
+
+
+    public Paralelogramma(double a) {
+        super(a);
+    }
+
+    @Override
+    public double getKerulet() {
+        return 0;
+    }
+
+    @Override
+    public double getTerulet() {
+        return 0;
+    }
+}
